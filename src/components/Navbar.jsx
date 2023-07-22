02547b4c6b39e909ab0a53547a941011239f1996
git_repo_user <evalgit@example.com>
2023-07-22 00:16:15 -0400

    css tweak

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,13 @@ import Logo from './ui/Logo';
 
 export default function Navbar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	const onHambugerClick = (e) => {
-		e.preventDefault();
+	const menuToggle = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
 
 	return (
 		<div className="w-full bg-black text-white md:flex md:px-4">
-			<div className="relative z-10 flex h-12 bg-black md:justify-normal px-4">
+			<div className="relative z-10 flex h-12 bg-black px-4 md:justify-normal">
 				<div className="basis-38 flex flex-shrink-0 items-center">
 					<Link to="/">
 						<Logo small /> <span className="antialiased">Recipes</span>
@@ -19,7 +18,7 @@ export default function Navbar() {
 				</div>
 				<button
 					className="group z-20 ml-auto flex h-12 w-12 flex-col items-end justify-center hover:bg-gray-800 active:bg-white md:hidden"
-					onClick={onHambugerClick}
+					onClick={menuToggle}
 				>
 					<div className="mx-auto mb-1 h-px w-6 bg-white group-active:bg-black"></div>
 					<div className="mx-auto mb-1 h-px w-6 bg-white group-active:bg-black"></div>
@@ -28,14 +27,15 @@ export default function Navbar() {
 			</div>
 			<div className="relative left-0 top-0 flex select-none items-center md:z-20">
 				<ul
-					className={`h-12 w-full items-center transition-all duration-300 ease-in-out max-md:absolute max-md:left-0 max-md:flex-row max-md:drop-shadow-md md:flex
-                    ${isMenuOpen ? 'top-0' : 'max-md:-top-[1000px]'}
+					className={`h-16 w-full items-center transition-all duration-300 ease-in-out max-md:absolute max-md:left-0 max-md:flex-row max-md:drop-shadow-md md:flex
+                    ${isMenuOpen ? 'top-0' : 'max-md:-top-[500px]'}
                   `}
 				>
 					<li className="h-full">
 						<Link
 							to="/"
-							className="flex h-full flex-col justify-center border-solid border-slate-400 px-3 text-center active:bg-slate-500 max-md:h-16 max-md:border-b max-md:bg-slate-700"
+							className="flex h-full flex-col justify-center border-solid border-slate-400 px-3 text-center active:bg-slate-500 max-md:border-b max-md:bg-slate-700"
+							onClick={menuToggle}
 						>
 							Home
 						</Link>
@@ -43,7 +43,8 @@ export default function Navbar() {
 					<li className="h-full">
 						<Link
 							to="/recipes"
-							className="flex h-full flex-col justify-center border-solid border-slate-400 px-3 text-center active:bg-slate-500 max-md:h-16 max-md:border-b max-md:bg-slate-700"
+							className="flex h-full flex-col justify-center border-solid border-slate-400 px-3 text-center active:bg-slate-500 max-md:border-b max-md:bg-slate-700"
+							onClick={menuToggle}
 						>
 							View All
 						</Link>
