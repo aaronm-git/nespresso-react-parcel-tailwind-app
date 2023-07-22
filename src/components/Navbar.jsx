4730bf0253625af04a6b3e460e75e80bc020f708
git_repo_user <evalgit@example.com>
2023-07-22 00:30:37 -0400

    styling updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,19 @@ export default function Navbar() {
 
 	return (
 		<div className="w-full bg-black text-white md:flex md:px-4">
-			<div className="relative z-10 flex h-12 bg-black px-4 md:justify-normal">
-				<div className="basis-38 flex flex-shrink-0 items-center">
+			<div className="relative z-10 mr-auto flex items-center bg-black max-md:px-4">
+				<div className="flex flex-shrink-0">
 					<Link to="/">
 						<Logo small /> <span className="antialiased">Recipes</span>
 					</Link>
 				</div>
 				<button
-					className="group z-20 ml-auto flex h-12 w-12 flex-col items-end justify-center hover:bg-gray-800 active:bg-white md:hidden"
+					className="group z-20 ml-auto flex h-16 w-16 flex-col items-end justify-center hover:bg-gray-800 active:bg-white md:hidden"
 					onClick={menuToggle}
 				>
 					<div className="mx-auto mb-1 h-px w-6 bg-white group-active:bg-black"></div>
 					<div className="mx-auto mb-1 h-px w-6 bg-white group-active:bg-black"></div>
-					<div className="mx-auto mb-1 h-px w-6 bg-white group-active:bg-black"></div>
+					<div className="mx-auto h-px w-6 bg-white group-active:bg-black"></div>
 				</button>
 			</div>
 			<div className="relative left-0 top-0 flex select-none items-center md:z-20">
@@ -46,7 +46,7 @@ export default function Navbar() {
 							className="flex h-full flex-col justify-center border-solid border-slate-400 px-3 text-center active:bg-slate-500 max-md:border-b max-md:bg-slate-700"
 							onClick={menuToggle}
 						>
-							View All
+							View Recipes
 						</Link>
 					</li>
 				</ul>
