03686386dcf2bbb4b0af792f60b25041e2f08d48
git_repo_user <evalgit@example.com>
2023-08-10 01:31:41 -0400

    updated browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { createHashRouter, createBrowserRouter, RouterProvider, HashRouter } from 'react-router-dom';
+import { createHashRouter, createBrowserRouter, RouterProvider, HashRouter, BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(
-	<HashRouter>
+	<BrowserRouter>
 		<App />
-	</HashRouter>,
+	</BrowserRouter>,
 );
