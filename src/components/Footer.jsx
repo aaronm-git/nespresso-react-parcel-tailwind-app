f43c30cc5cae7e97b1203692cfe5729d22152ee6
aaronmolina <28906210+aaronm-git@users.noreply.github.com>
2023-08-25 13:45:37 -0400

    added sidebar context to enable auto close on clicked items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
 		<footer className="fixed bottom-0 left-0 z-10 w-full bg-black py-3 text-center text-white">
 			<Container>
 				<small>
-					Brewed with love and code, this project represents my passion for both coffee and coding. You can
-					find more of my work at <a href="https://aaronmolina.me">aaronmolina.me</a>
+					Brewed with love, this project represents my passion for both coffee and coding. You can find more
+					of my work at <a href="https://aaronmolina.me">aaronmolina.me</a>
 				</small>
 			</Container>
 		</footer>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, createContext, useContext } from 'react';
 import FeatherIcon from 'ui/FeatherIcon';
 
-function Sidebar({ children }) {
+const SidebarContext = createContext({
+	isSidebarOpen: false,
+	toggleSidebar: () => {},
+});
+
+const SidebarProvider = ({ children }) => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 	const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+	return <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>{children}</SidebarContext.Provider>;
+};
+
+function Sidebar({ children }) {
+	return (
+		<SidebarProvider>
+			<Nav>{children}</Nav>
+		</SidebarProvider>
+	);
+}
+
+function Nav({ children }) {
+	const { isSidebarOpen, toggleSidebar } = useContext(SidebarContext);
 	return (
 		<>
 			<StickyMenuButton toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
 			<nav
-				className="group fixed top-0 z-10 h-full w-[200px] max-w-[200px] translate-x-0 transform overflow-x-hidden bg-gray-200 pb-[96px] pt-[64px] transition-all duration-300 ease-in-out data-[issidebaropen=true]:shadow max-md:data-[issidebaropen=false]:-translate-x-full md:pb-[72px] lg:pb-[48px]"
+				className="group fixed top-0 z-10 h-full w-[200px] max-w-[200px] translate-x-0 transform overflow-x-hidden border-r border-gray-300 bg-gray-200 pb-[96px] pt-[64px] transition-all duration-300 ease-in-out data-[issidebaropen=true]:shadow max-md:data-[issidebaropen=false]:-translate-x-full md:pb-[72px] lg:pb-[48px]"
 				data-issidebaropen={isSidebarOpen}
 			>
 				<div className="border-b border-solid border-gray-300 p-3 py-5 text-center">
@@ -28,16 +46,22 @@ function Menu({ children }) {
 }
 
 function Item({ children }) {
-	return <li className="border-b border-solid border-gray-300 bg-gray-200 p-3 px-5">{children}</li>;
+	const { toggleSidebar } = useContext(SidebarContext);
+	return (
+		<li className="border-b border-solid border-gray-300 bg-gray-200 p-3 px-5" onClick={toggleSidebar}>
+			{children}
+		</li>
+	);
 }
 
 function StickyMenuButton({ toggleSidebar, isSidebarOpen }) {
 	return (
-		<div id="sidebarStickyMenuButton" className="absolute bottom-[100px] right-3 p-2">
-			<button className="rounded-full bg-nespresso-gold p-3 text-white" onClick={toggleSidebar}>
-				<FeatherIcon icon={isSidebarOpen ? 'x' : 'layout'} />
-			</button>
-		</div>
+		<button
+			className="absolute bottom-[115px] right-5 rounded-full bg-nespresso-gold p-4 text-white shadow md:hidden"
+			onClick={toggleSidebar}
+		>
+			<FeatherIcon icon={isSidebarOpen ? 'x' : 'layout'} />
+		</button>
 	);
 }
 
